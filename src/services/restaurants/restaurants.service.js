b08c64f2d1c0d44d6ff69d8a2baeee672fa72520
Antonio Sucre <ajs21br@gmail.com>
2022-01-22 07:17:56 -0300

    Let's Transform the Data

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,4 +1,6 @@
 import { mocks } from './mock';
+import camelize from 'camelize';
+
 export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
     return new Promise((resolve, reject) => {
         const mock = mocks[location];
@@ -9,8 +11,15 @@ export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
     })
 }
 
-restaurantsRequest().then((result) => {
-    console.log(result);
-}).catch((err) => {
-    console.log("error")
-});
+const restaurantsTransform = (result) => {
+    const newResult = camelize(result);
+    //transformData
+    return newResult;
+}
+restaurantsRequest()
+    .then(restaurantsTransform)
+    .then((transformedResponse) => {
+        console.log(transformedResponse);
+    }).catch((err) => {
+        console.log("error")
+    });
