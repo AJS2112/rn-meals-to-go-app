53a7f3830cf4966956438a27c1508ceecebea0db
Antonio Sucre <ajs21br@gmail.com>
2022-01-23 06:40:03 -0300

    Adding Activity Container

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -4,6 +4,7 @@ import { View, SafeAreaView, StatusBar, FlatList } from "react-native";
 import { Searchbar } from 'react-native-paper';
 import { RestaurantInfoCard } from '../components/restaurant-info-card-component';
 import { Spacer } from "../../../components/spacer/spacer.component";
+import { ActivityIndicator, Colors } from 'react-native-paper';
 
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 
@@ -20,11 +21,31 @@ const RestaurantListContainer = styled(FlatList)`
     padding: ${(props) => props.theme.space[3]};
 `;
 
+const Loading = styled(ActivityIndicator)`
+margin-left:-25px
+`;
+
+const LoadingContainer = styled.View`
+    position:absolute;
+    top:50%;
+    left:50%;
+`;
+
 export const RestaurantScreen = (props) => {
     const { restaurants, isLoading, error } = useContext(RestaurantsContext);
     return (
         <>
             <SafeArea>
+                {isLoading && (
+                    <LoadingContainer>
+                        <Loading
+                            size={50}
+                            style={{ marginLeft: -25 }}
+                            animating={true}
+                            color={Colors.blue300}
+                        />
+                    </LoadingContainer>
+                )}
                 <SearchContainer>
                     <Searchbar />
                 </SearchContainer>
